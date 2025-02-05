c5808a0b89f590ae1b87cf87fa23c2bf3eabd5ec
KabsiMontassar <kebsimontassar@gmail.com>
2025-02-05 21:47:45 +0100

    Add project files.

---
+++ Class Library/Domain/Flight.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Library.Domain
+{
+    class Flight
+    {
+        public string Departure { get; set; }
+        public string Destination { get; set; }
+        public DateTime EffectiveArrival { get; set; }
+        public int EstimatedDuration { get; set; }
+        public DateTime FlightDate { get; set; }
+        public int FlightId { get; set; }
+        
+    }
+}
---
+++ Class Library/Domain/Plane.cs
@@ -0,0 +1,13 @@
+﻿namespace Class_Library.Domain
+{
+    public class Plane
+    {
+        public string Departure { get; set; }
+        public string Destination { get; set; }
+        public DateTime EffectiveArrival { get; set; }
+        public int EstimatedDuration { get; set; }
+        public DateTime FlightDate { get; set; }
+        public int FlightId { get; set; }
+
+    }
+}
